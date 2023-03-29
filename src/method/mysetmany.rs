15da0b41bfa34c0250968de8c9fa4a35bfb96a6d
zomem <770552117@qq.com>
2023-03-29 18:12:02 +0800

    man

--- src/method/mysetmany.rs
+++ src/method/mysetmany.rs
@@ -1,5 +1,6 @@
 
 
+
 /// 批量新增数据 ，返回 sql 语句。
 /// 下面示例中，user 为表名，，name、num 为字段名，，后面为新增的值。
 /// Add new data, then return sql. exp: users table, field name and num. after them is value.
@@ -16,6 +17,23 @@
 /// ]);
 /// 
 /// my_run_drop(&mut sql).unwrap();
+/// 
+/// 
+/// 也可以通过 Vec<T> 的方式批量新增：
+/// // 注意：此方法不适合字符串里面有 ," 这个连续出现的情况。如 "abc,"def"
+/// 
+/// #[derive(Serialize, Deserialize, Debug)]
+/// struct Item {
+///     content: String,
+///     total: u32,
+///     uid: u8,
+///     des: String
+/// }
+/// let vec_data = vec![
+///     Item {content: String::from("aaa"), total: 12, uid: 3, des: String::from("nn")},
+///     Item {content: String::from("bb"), total: 1, uid: 3, des: String::from("aaa")},
+/// ];
+/// let sql = mysetmany!("content", vec_data);
 /// ```
 #[macro_export]
 macro_rules! mysetmany {
@@ -65,4 +83,46 @@ macro_rules! mysetmany {
             sql
         }
     };
+
+
+    ($t:expr, $v: expr) => {
+        {
+            fn type_of<T>(_: T) -> &'static str {
+                std::any::type_name::<T>()
+            }
+            let mut field_name = " (".to_string();
+            let mut value = "".to_string();
+            for i in 0..$v.len() {
+                let mut item_str = serde_json::to_string(&$v[i]).unwrap();
+                item_str.pop();
+                item_str.remove(0);
+                item_str.push(',');
+                item_str.push('"');
+                item_str.insert(0, ',');
+                // ",\"content\":\"aaa\",\"total\":12,\"uid\":3,\"des\":\"nn\",\""
+                value = value + " (";
+                let re2 = regex::Regex::new("\":(.*?),\"").unwrap();
+                for cap2 in re2.captures_iter(item_str.as_str()) {
+                    value = value + &cap2[1] + ",";
+                }
+                value.pop();
+                value = value + "),";
+
+                if i == 0 {
+                    let re = regex::Regex::new(",\"([0-9a-zA-Z_]+?)\":").unwrap();
+                    for cap in re.captures_iter(item_str.as_str()) {
+                        field_name = field_name + &cap[1] + ",";
+                    }
+                    field_name.pop();
+                    field_name = field_name + ")";
+                }
+            }
+            value.pop();
+            let sql: String = "INSERT INTO ".to_string() + $t + field_name.as_str()
+                + " VALUES" + value.as_str();
+
+            sql
+        }
+    };
+
 }
